f03f4fc61496b140f6d07cfaf959566ae008296f
v <v@mail.local>
2026-09-02 13:49:01 +0000

    Created a template for the attacking library.

---
+++ basic-cryptography-demo/Attack.cs
@@ -0,0 +1,10 @@
+namespace basic_cryptography_demo;
+
+public static class Attack
+{
+    // Assumes that the messages have been encrypted with the same key.
+    public static string[] AttackSameKey(string[] messages)
+    {
+        throw new NotImplementedException();
+    }
+}
